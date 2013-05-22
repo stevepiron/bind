fb62907b19af15c3e4bbb72816c2d52d63de5581
Steve <piron.steve@gmail.com>
2013-05-22 07:22:32 +0200

    Email pour beta
    Signed-off-by: Steve <piron.steve@gmail.com>

--- php/signup-email.inc.php
+++ php/signup-email.inc.php
@@ -2,7 +2,7 @@
 
 	$to = $email;
 	$subject = 'Active ton compte Bind dès maintenant !';
-	$message = 'Clique sur ce lien pour activer ton compte: http://bind.stevepiron.be/index.php?page=activation&hash='.$hash;
+	$message = 'Clique sur ce lien pour activer ton compte: http://beta.bind.stevepiron.be/index.php?page=activation&hash='.$hash;
 	$headers = 'From:steve@bind.com'. "\r\n";
 	mail($to, $subject, $message, $headers);
 	
