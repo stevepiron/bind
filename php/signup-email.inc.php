c54e6735c8f2364a7b898d1f71954b2e0dc0e496
Steve <piron.steve@gmail.com>
2013-06-11 23:09:10 +0200

    Mail d'activation
    Signed-off-by: Steve <piron.steve@gmail.com>

--- php/signup-email.inc.php
+++ php/signup-email.inc.php
@@ -2,8 +2,18 @@
 
 	$to = $email;
 	$subject = 'Active ton compte Bind dès maintenant !';
-	$message = 'Clique sur ce lien pour activer ton compte: http://beta.bind.stevepiron.be/index.php?page=activation&hash='.$hash;
-	$headers = 'From:steve@bind.com'. "\r\n";
+	
+	$message  = '<html><body>';
+	$message .= '<p>Salut et bienvenue sur Bind !</p>';
+	$message .= '<p>Bind te permet d\'entrer et rester en contact avec tous les étudiants de ton école.</p>';
+	$message .= '<p>Maintenant que tu fais partie de la famille, tu peux dès à présent adresser une question qui sera transmise à une incroyable quantité de matière grise !</p>';
+	$message .= '<p>Si tu as un peu de temps libre, reviens nous voir et partage ton savoir, il y a de fortes chances que celui-ci te rapporte de précieux points pour lesquels tes potes se battent déjà ;)</p>';
+	$message .= '<p>En voiture Simone ! <a href="http://bind.stevepiron.be/index.php?page=activation&hash='.$hash.'">Active ton compte</a> tant qu\'il est encore chaud !</p>';
+	$message .= '</body></html>';
+	
+	$headers  = 'From:steve@bind.com'."\r\n";
+	$headers .= 'MIME-Version: 1.0'."\r\n";
+	$headers .= 'Content-Type: text/html; charset=utf-8'."\r\n";
 	mail($to, $subject, $message, $headers);
 	
 
